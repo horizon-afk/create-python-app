b071b1b310478e9d5a488d8dc7e5c930bef81a0c
Umesh <umeshsarkar19@outlook.com>
2024-06-15 20:08:47 +0530

    Basic python apps with venv is implemented

--- src/main.rs
+++ src/main.rs
@@ -22,9 +22,15 @@ fn main() {
 
     let interface : Interface = Interface::parse();
 
+    // creating the app directory
     let dir_name = interface.directory;
     fs::create_dir(dir_name.clone()).expect("Invalid filename");
     fs::File::create_new(dir_name.clone()+ "/app.py").expect("File already exists");
+    env::set_current_dir(&dir_name).expect("Error! Can't change current directory");
 
+    // creating the virtual environment
+    let mut venv = Command::new("python");
+    venv.args(["-m", "virtualenv", ".venv"]).spawn()
+        .expect("Could not create venv. Download virtualenv and try again.");
     
 }
