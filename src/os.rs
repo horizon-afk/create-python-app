a692d0f3110361badf91adcdab5e2cbe579e6168
Umesh <umeshsarkar19@outlook.com>
2024-07-08 19:45:59 +0530

    Refactored error messages

--- src/os.rs
+++ src/os.rs
@@ -3,13 +3,17 @@ use std::fs;
 use std::fs::File;
 
 pub fn mkdir(folder_name: String) {
-    fs::create_dir(folder_name).expect("Error! " + &folder_name + "already exists");
+    fs::create_dir(&folder_name).expect(
+        (String::from("Error ") + &folder_name + &String::from(" already exists")).as_str(),
+    );
 }
 
 pub fn mkfile(file_name: String) -> File {
-    fs::File::create_new(file_name).expect("Error! File already exists")
+    fs::File::create_new(&file_name)
+        .expect((String::from("Error! ") + &file_name + &String::from("already exists")).as_str())
 }
 
 pub fn cwd(dir_name: String) {
-    env::set_current_dir(&dir_name).expect("Unable to change directory");
+    env::set_current_dir(&dir_name)
+        .expect((String::from("Unable to change directory to ") + &dir_name).as_str());
 }
